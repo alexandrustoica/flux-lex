178dbef47aeafe0bfb91f12845a94f53de75e1d8
Alexandru Stoica <stoica.alexandru2000@gmail.com>
2018-12-02 14:19:29 +0200

    HotFix: Gradle Build Configuration

---
+++ src/main/kotlin/org/flux/application/FluxArgs.kt
@@ -0,0 +1,8 @@
+package org.flux.application
+
+import com.xenomachina.argparser.ArgParser
+
+class FluxArgs(parser: ArgParser) {
+    val source by parser.positional("SOURCE", help = "source filename")
+
+}
--- src/main/kotlin/org/flux/application/Main.kt
+++ src/main/kotlin/org/flux/application/Main.kt
@@ -1,10 +1,7 @@
+package org.flux.application
 import com.xenomachina.argparser.ArgParser
 import flux.analyzer.LexicalAnalyzer
 import java.io.File
-
-class FluxArgs(parser: ArgParser) {
-    val source by parser.positional("SOURCE", help = "source filename")
-}
 
 fun main(args: Array<String>) {
     ArgParser(args).parseInto(::FluxArgs).run {
