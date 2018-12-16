535fed2172e56488e50639fe897ba7df7aa987bc
alexandrustoica <stoica.alexandru2000@gmail.com>
2018-12-16 10:07:46 +0200

    Args Improvement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,15 @@
-
+import com.xenomachina.argparser.ArgParser
 import flux.analyzer.LexicalAnalyzer
 import java.io.File
 
-
+class FluxArgs(parser: ArgParser) {
+    val source by parser.positional("SOURCE", help = "source filename")
+}
 
 fun main(args: Array<String>) {
-    val analyzer = LexicalAnalyzer(File("source.flux")).analyze()
-    analyzer.validator.accumulator.forEach { println(it.message)}
-    analyzer.internalForm.forEach { println("${it.first} | ${it.second}") }
+    ArgParser(args).parseInto(::FluxArgs).run {
+        val analyzer = LexicalAnalyzer(File(source)).analyze()
+        analyzer.validator.accumulator.forEach { println(it.message) }
+        analyzer.internalForm.forEach { println("${it.first} | ${it.second}") }
+    }
 }
